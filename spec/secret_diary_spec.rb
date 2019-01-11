70f354797c90a8990c890754ebb07176e3ae4cb4
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-11 11:19:24 +0000

    rspec added

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,1 +1,8 @@
 require "secret_diary.rb"
+
+describe DockingStation do
+  it { is_expected.to respond_to :add_entry }
+  it { is_expected.to respond_to :get_entry }
+  it { is_expected.to respond_to :lock }
+  it { is_expected.to respond_to :unlock }
+end
