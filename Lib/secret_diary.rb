ecb9eea44dbeb5b525a72033a7343741e9f73a75
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-11 11:22:52 +0000

    rspec passed

---
+++ Lib/secret_diary.rb
@@ -0,0 +1,16 @@
+class Diary
+  def initialize
+  end
+
+  def add_entry
+  end
+
+  def get_entry
+  end
+
+  def lock
+  end
+
+  def unlock
+  end
+end
--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -1,6 +1,6 @@
 require "secret_diary.rb"
 
-describe DockingStation do
+describe Diary do
   it { is_expected.to respond_to :add_entry }
   it { is_expected.to respond_to :get_entry }
   it { is_expected.to respond_to :lock }
